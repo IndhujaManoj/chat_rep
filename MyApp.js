f0416745735a554f1b96d2e98e80e34fc6725603
IndhujaManoj <manojindhuja6@gmail.com>
2023-05-08 12:13:24 +0530

    Chat App

--- MyApp.js
+++ MyApp.js
@@ -6,7 +6,7 @@ function hideReg() {
     document.getElementById('page2').style.display = "none"
     document.getElementById('page1').style.display = "block"
 }
-function loading() {
+function homePage() {
     document.getElementById('page1').style.display = "none"
     document.getElementById('page4').style.display = "block"
 }
@@ -24,6 +24,11 @@ function inputClear() {
     document.getElementById("psw").value = ""
     document.getElementById("mail").value = ""
     document.getElementById("name").value = ""
+    document.getElementById('n1').innerHTML=""
+    document.getElementById('n2').innerHTML=""
+    document.getElementById('n3').innerHTML=""
+    document.getElementById('n4').innerHTML=""
+    document.getElementById('n4').innerHTML=""
 }
 function newsLogout(){
     document.getElementById('page6').style.display='none'
@@ -62,7 +67,7 @@ function loginHandling(event) {
         if (user.username === username && user.password === password) {
             index = regUsers.indexOf(user)
             alert('login successfull!')
-            loading()
+            homePage()
             foundUser = true;
             break;
         }
@@ -71,74 +76,54 @@ function loginHandling(event) {
         alert('Invalid username or password');
     }
 }
-//register form
-
+//________________________________________________________________________________REGISTATION FORM__________________________________________________________________
 let regUsers = []
-function registation(event) {
-    event.preventDefault()
-    //username
-    var username = document.getElementById("name").value;
-    var checkUsername = /^[a-zA-Z\s]/;
-    var valuate = checkUsername.test(username)
-    if (valuate == false) {
-        document.getElementById("n1").innerHTML = "Please enter a Valid Username"
+function registerUser(event) {
+    event.preventDefault();
+  
+    const nameInput = document.getElementById("name");
+    const emailInput = document.getElementById("mail");
+    const passwordInput = document.getElementById("psw");
+    const repasswordInput = document.getElementById("new1");
+    const phoneInput = document.getElementById("no");
+  
+    const nameValue = nameInput.value.trim();
+    const emailValue = emailInput.value.trim();
+    const passwordValue = passwordInput.value.trim();
+    const repasswordValue = repasswordInput.value.trim();
+    const phoneValue = phoneInput.value.trim();
+  
+    let error = 0;
+    if (!nameValue) {
+     document.getElementById("n1").innerHTML = "Please enter a Valid Username"
+     error++
     }
-    else if (valuate == true) {
-        document.getElementById("n1").innerHTML = ""
+    if (!emailValue.match(/^\S+@\S+\.\S+$/)) {
+     document.getElementById("n2").innerHTML = "Please enter a Valid email"
+error++
     }
-
-    //email-id
-    var email = document.getElementById("mail").value;
-    let checkEmail = /^\S+@\S+\.\S/;
-    var valuate2 = checkEmail.test(email)
-    if (valuate2 == false) {
-        document.getElementById("n2").innerHTML = "Please enter a Valid mail-Id"
+    if (!passwordValue.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[#$@!%&?])[A-Za-z\d#$@!%&?]{8,30}$/)) {
+        document.getElementById("n3").innerHTML ="Password must contain at least 1 uppercase, 1 lowercase, 1 special character, and 1 digit, and be between 8 and 30 characters"
+    error++
     }
-    else if (valuate2 == true) {
-        document.getElementById("n2").innerHTML = ""
+    if (passwordValue !== repasswordValue) {
+        document.getElementById("n4").innerHTML ="Passwords do not match"
+    error++
     }
-
-    //password
-    var password = document.getElementById("psw").value;
-    let checkPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[#$@!%&?])[A-Za-z\d#$@!%&?]{8,30}$/;
-    var valuate3 = checkPassword.test(password);
-    if (valuate3 == false) {
-        document.getElementById("n3").innerHTML = "*password must contain min 1('uppercase','lowercase','special charcter','no') & '8-30' characters."
+    if (!phoneValue.match(/^[\d]{10}$/)) {
+        document.getElementById("n5").innerHTML ="Please enter a 10-digit phone number"
+    error++
     }
-    else if (valuate3 == true) {
-        document.getElementById("n3").innerHTML = ""
-    }
-
-    //re-enter password
-    var repassword = document.getElementById("new1").value;
-    if (password != repassword || repassword == "") {
-        document.getElementById("n4").innerHTML = "please enter correct password"
-    }
-    else if (password == repassword) {
-        document.getElementById("n4").innerHTML = ""
-    }
-
-    //phone number
-    var phoneno = document.getElementById("no").value;
-    var checkPhoneno = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}/;
-    var valuate4 = checkPhoneno.test(phoneno);
-    if (valuate4 == false) {
-        document.getElementById("n5").innerHTML = "please enter your 10 digit mobile no without +91"
-    }
-    else if (valuate4 == true) {
-        document.getElementById("n5").innerHTML = ''
-    }
-
-    if (valuate == true && valuate2 == true && valuate3 == true && valuate4 == true && password == repassword) {
-        f3 = document.getElementById('f3')
-        var myobj = { username: username, password: password }
-        regUsers.push(myobj)
-        console.log(regUsers, "reg")
-    }
-    inputClear()
-
-    hideReg()
-}
+    if (error!= 0) {
+        alert('please fill valid information')
+    } else {
+      const newUser = {username: nameValue,password: passwordValue};
+      regUsers.push(newUser);
+      console.log(regUsers, "reg");
+      inputClear();
+      hideReg();
+    } }
+//__________________________________________________________________GROUP CHAT____________________________________________________________________________________________
 var messageInput = document.getElementById("message-input");
 let logoutButton = document.getElementById('logout-button')
 var username = document.getElementById('uname').value;
@@ -185,8 +170,7 @@ function messageInputfun() {
     display()
 
 }
-
-//news
+//_____________________________________________________________NEWS____________________________________________________________________________________________
 let imgNews = document.getElementById('img2')
 imgNews.addEventListener('click', newShow)
 function newShow() {
@@ -264,8 +248,8 @@ fetch('https://api.rss2json.com/v1/api.json?rss_url=https://timesofindia.indiati
         })
     })
     .catch(error => console.error(error));
+    //___________________________________________________________________PROFILE CHANGE_________________________________________________________________________________________
 
-//profile
 let profileimg = document.getElementById('avatar');
 profileimg.addEventListener('click', profileShow)
 function profileShow() {
